57b55027f492fbf1e3011d56852746eec78d5f9c
KIM DONGUK <kampo5096@hanyang.ac.kr>
2024-09-02 07:21:24 +0000

    섹션 7. JPA 라이브러리 셋팅 및 JPA 응용 레포지토리 구성

--- spring-study/src/main/java/com/study/spring_study/SpringConfig.java
+++ spring-study/src/main/java/com/study/spring_study/SpringConfig.java
@@ -6,20 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.study.spring_study.repository.MemoryMemberRepository;
 import com.study.spring_study.repository.JdbcMemberRepository;
 import com.study.spring_study.repository.JdbcTemplateMemberRepository;
+import com.study.spring_study.repository.JpaMemberRepository;
 import com.study.spring_study.repository.MemberRepository;
-import com.study.spring_study.repository.MemoryMemberRepository;
+
 import com.study.spring_study.service.MemberService;
+
+import jakarta.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    // private final DataSource dataSource;
+    private final EntityManager em;
 
+    // EntityManager 클래스 스프링 빈으로 의존성 삽입 (Dependency Injection | DI)
     @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -31,7 +37,8 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
--- spring-study/src/main/java/com/study/spring_study/domain/Member.java
+++ spring-study/src/main/java/com/study/spring_study/domain/Member.java
@@ -1,10 +1,20 @@
 package com.study.spring_study.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /*
  *   회원 도메인
  */
+
+
+ @Entity
 public class Member {
 
+    // PK Index 설정
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
