689961eea2d345f8eb067ef63d7c583df4ca3b4c
KIM DONGUK <kampo5096@hanyang.ac.kr>
2024-09-28 11:31:08 +0000

    섹션 7. 웹 계층 개발 - 회원 목록조회

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -6,6 +6,9 @@ import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,25 +23,43 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /**
+     * 회원가입 뷰 전환
+     */
     @GetMapping("/members/new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm()); // Controller -> View (Model 기반으로 하여금 데이터 이동)
         return "members/createMemberForm";
     }
 
+    /**
+     * 회원 생성
+     */
     @PostMapping("/members/new")
     public String createMember(@Valid MemberForm form, BindingResult result) {
-        if(result.hasErrors()) { //thymeleaf 엔진과 스프링 부트는 Integrate 높게 형성됨.
+        if (result.hasErrors()) { //thymeleaf 엔진과 스프링 부트는 Integrate 높게 형성됨.
             return "members/createMemberForm";
         }
 
+        Member member = new Member();
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
 
-        Member member = new Member();
         member.setAddress(address);
         member.setName(form.getName());
         memberService.join(member);
 
         return "redirect:/";
     }
+
+    /**
+     * 회원 목록조회
+     */
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        log.info("is this passed?");
+        return "members/memberList";
+    }
+    
 }
