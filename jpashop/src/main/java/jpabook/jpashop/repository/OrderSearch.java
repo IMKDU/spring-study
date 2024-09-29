644be5d1ddf3fec3c37087ced7cca476f8b8caf9
KIM DONGUK <kampo5096@hanyang.ac.kr>
2024-09-29 14:45:28 +0000

    섹션 7. 웹 계층 개발 - 주문목록 검색, 취소

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-public class OrderSerch {
+public class OrderSearch {
 
     private String memberName; //회원이름
     private OrderStatus orderStatus; // 주문상태[ORDER, CANCEL]
