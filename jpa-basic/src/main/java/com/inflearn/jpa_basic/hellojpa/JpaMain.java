b199f395e0f45254e92d42f15751dc8a361eb902
KIM DONGUK <kampo5096@hanyang.ac.kr>
2024-10-01 11:32:39 +0000

    섹션 2. JPA 프로젝트 셋팅 (with Gradle)

---
+++ jpa-basic/src/main/java/com/inflearn/jpa_basic/hellojpa/JpaMain.java
@@ -0,0 +1,20 @@
+package com.inflearn.jpa_basic.hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        //code
+        em.close();
+
+        emf.close();
+
+    }
+
+}
